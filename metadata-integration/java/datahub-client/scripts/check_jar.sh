089363f5688d48618293b47bed68f12b5251eda3
MugdhaHardikar-GSLab <mugdha.hardikar@gslab.com>
2022-06-07 10:02:59 +0530

    Update check_jar.sh

--- metadata-integration/java/datahub-client/scripts/check_jar.sh
+++ metadata-integration/java/datahub-client/scripts/check_jar.sh
@@ -7,7 +7,6 @@ jar -tvf $jarFile |\
       grep -v "include/" |\
       grep -v "linux/" |\
       grep -v "darwin" |\
-      grep -v "aix/" |\
       grep -v "datahub/shaded" |\
       grep -v "META-INF" |\
       grep -v "com/linkedin" |\
